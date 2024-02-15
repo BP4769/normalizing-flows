0a6599be0c8976dcc18a6c2f0acad9151656d7ff
BP4769 <57891977+BP4769@users.noreply.github.com>
2024-02-15 12:25:37 +0100

    shapes init

---
+++ test/__init__.py
@@ -0,0 +1,12 @@
+from test.shapes import (
+    generate_continuously_colored_samples,
+    generate_grid_data,
+    caret,
+    circle,
+    double_circle,
+    n_regular_polygon,
+    grid,
+    moons,
+    swiss_roll,
+    swirl
+)
